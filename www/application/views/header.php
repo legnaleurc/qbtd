ee1d64914c6dacf2e6ea73cb43098dab924531a8
Zero <john.royer@gmail.com>
2012-06-01 14:56:53 +0800

    Fix css path error
    Browser can not get css/javascript if only relative path set. Use
    base_url() in URL helper to create absolute path for css.

--- www/application/views/header.php
+++ www/application/views/header.php
@@ -1,9 +1,10 @@
+<?php  $this->load->helper('url'); ?>
 <html>
    <head>
       <meta http-equiv="content-type" content="text/html; charset=utf-8">
 
       <title>bootstrap test</title>
-      <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" charset="utf-8">
-      <link rel="stylesheet" href="css/prim.css" type="text/css" charset="utf-8">
+      <link rel="stylesheet" href="<?php echo base_url(); ?>/css/bootstrap.min.css" type="text/css" charset="utf-8">
+      <link rel="stylesheet" href="<?php echo base_url(); ?>/css/prim.css" type="text/css" charset="utf-8">
    </head>
    <body>
