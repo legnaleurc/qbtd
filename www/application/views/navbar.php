2f6f410daf96bbfc9e18abbe8eef62f87be5016b
Zero <john.royer@gmail.com>
2012-06-11 15:09:41 +0800

    Add URL for navbar links

--- www/application/views/navbar.php
+++ www/application/views/navbar.php
@@ -1,17 +1,17 @@
 <div class="navbar navbar-fixed-top">
    <div class="navbar-inner">
       <div class="container">
-         <a class="brand" href="#">Qbtd</a>
+      <a class="brand" href="<?php echo site_url(); ?>">Qbtd</a>
 
 <?php if( $loggedin == true ): ?>
             <ul class="nav pull-right" >
-               <li><a href="#logout">Logout</a></li>
+            <li><a href="<?php echo site_url('user/logout'); ?>">Logout</a></li>
             </ul>
 
 <?php else: ?>
             <ul class="nav pull-right" >
-               <li><a href="#reg">Regieter</a></li>
-               <li><a href="#login">Login</a></li>
+            <li><a href="<?php echo site_url('user/'); ?>">Regieter</a></li>
+            <li><a href="<?php echo site_url('user/'); ?>">Login</a></li>
             </ul>
 <?php endif; ?>
       </div>
