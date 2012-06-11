e72aaa86a581a298453537188cbbecc88ed0c176
Zero <john.royer@gmail.com>
2012-06-11 18:00:26 +0800

    The image should not be here

--- www/application/views/home_welcome.php
+++ www/application/views/home_welcome.php
@@ -6,9 +6,6 @@
    <h1>Qbtd</h1>
    <p>BT download agent.</p>
 
-   <p style="width: 750px; margin: 50px auto;"><img src="<?php echo base_url(); ?>/img/2012-05-26_204933.png" /></p>
-
-
 <?php
    $this->load->view('footer');
 ?>
