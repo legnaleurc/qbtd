1522767b72f0f20ce3c47179de77b65523c04b94
Zero <john.royer@gmail.com>
2012-06-16 22:29:22 +0800

    Add Tabs for User Management Panel

--- www/application/views/admin/home.php
+++ www/application/views/admin/home.php
@@ -2,6 +2,22 @@
 <?php  $this->load->view('navbar');  ?>
 <?php  $this->load->helper('url');  ?>
 
+<div class="row">
+<div class="span12">
 <h2>User Management</h2>
+<p style="margin-top: 25px;"> </p>
+<ul class="nav nav-tabs ">
+   <li class="active"><a href="#">General</a></li>
+   <li><a href="#">Admin</a></li>
+   <li>
+      <form class="form-search admin-tabs-search" action="#" method="post" accept-charset="utf-8">
+      <i class="icon-search admin-tabs-search-icon"></i><input class="" type="text" name="key" value="">
+      </form>
+   </li>
+</ul>
+
+
+</div>
+</div>
 
 <?php $this->load->view('footer');  ?>
