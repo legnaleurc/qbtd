cd7687705f759860cf393ec3a1abba1910baddfb
Zero <john.royer@gmail.com>
2012-06-30 15:32:05 +0800

    Move prim.css to style.css

--- www/application/views/header.php
+++ www/application/views/header.php
@@ -6,7 +6,7 @@
 
       <title><?php echo $page_title; ?></title>
       <link rel="stylesheet" href="<?php echo base_url(); ?>/css/bootstrap.min.css" type="text/css" charset="utf-8">
-      <link rel="stylesheet" href="<?php echo base_url(); ?>/css/prim.css" type="text/css" charset="utf-8">
+      <link rel="stylesheet" href="<?php echo base_url(); ?>/css/style.css" type="text/css" charset="utf-8">
    </head>
    <body>
 
