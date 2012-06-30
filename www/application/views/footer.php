f203818fd605ba2345bb5850d7bae8109e75b087
Zero <john.royer@gmail.com>
2012-06-30 15:17:16 +0800

    Import jQuery and plugins

--- www/application/views/footer.php
+++ www/application/views/footer.php
@@ -1,6 +1,7 @@
 </div>
 
-<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+<script src="<?php echo base_url(); ?>/js/jquery-1.7.2.min.js"></script>
+<script src="<?php echo base_url(); ?>/js/jquery.validate.min.js"></script>
 <script src="<?php echo base_url(); ?>/js/bootstrap.min.js"></script>
 </body>
 </html>
